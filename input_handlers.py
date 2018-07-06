eecc3cf3b82a151d1caad265686d0b0d70a1656f
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2018-07-06 08:55:29 +0100

    Initial commit. Added basic display engine, and control module 'input_handlers.py'

---
+++ input_handlers.py
@@ -0,0 +1,24 @@
+import libtcodpy as libtcod
+
+
+def handle_keys(key):
+    # movement keys
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1, 0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # ALT + ENTER: toggle full-screen
+        return {'fullscreen': True}
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # ESC: Exit game
+        return {'exit': True}
+
+    # No Key Pressed: do nothing
+    return {}
