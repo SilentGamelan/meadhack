7022ce6b04e6adda27a49afc9fcfa69becbdd7cf
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2018-07-09 20:05:06 +0100

    Added basic room and corridor building functions and helpers

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,7 +1,10 @@
 # game_map.py 
-#
-# Defines the game map as a 2-d array, performs initalisation, and describes methods for interacting with the map object
 
+"""
+Defines the game map as a 2-d array, performs initalisation, and describes methods for interacting with the map object
+The dungeon generation algorithm will default to all blocked tiles, and will "dig out" moveable sections instead
+"""
+from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
 class GameMap:
@@ -12,19 +15,41 @@ class GameMap:
 
     # Initially creates all tiles to be NON-BLOCKING
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
-        # TESTING - alter some tiles's properties. Remove this later.
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
-        tiles[0][0].blocked = True
         return tiles
+    
+    def make_map(self):
+    # 2 sample rooms for demonstration purposes
+        room1 = Rect(20, 15, 10, 15)
+        room2 = Rect(35, 15, 10, 15)
+
+        self.create_room(room1)
+        self.create_room(room2)
+
+        self.create_h_tunnel(25, 40, 23)
+
+
+
+    def create_room(self, room):
+        # Make each tile in rectangle passable
+        # The +1 accounts for an impassible wall around the room
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
             return True
-        return False
+        return False   
