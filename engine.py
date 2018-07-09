004f3bf43dbcdcd13cb4c3b82485fcff8e54fc2d
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2018-07-09 17:04:29 +0100

    Fixed module import problems by removing __init__,py as not required in py3. Implemented basic collision detection using game_map.is_blocked() method call. Fixed problem with missing map size variables, had altered screen sizes instead of adding map_height and map_width

--- engine.py
+++ engine.py
@@ -9,9 +9,11 @@ from render_functions import clear_all, render_all
 def main():
 
     screen_width = 80
-    # DEBUG - this appears to have been changed from 50 but I don't remember seeing it being specified earlier in tutorial
     screen_height = 45
-
+    
+    map_width = 80
+    map_height = 45
+    
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150)
@@ -48,7 +50,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
 
         if exit:
